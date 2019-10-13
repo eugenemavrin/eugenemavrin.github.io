b885825df877f9ef2ea04a9bea3133571abcbc63
Eugene Mavrin <eugene.mavrin@gmail.com>
2019-10-13 05:03:39 +0300

    Delete Milena.js

--- Milena.js
+++
@@ -1,8 +0,0 @@
-var synth = window.speechSynthesis;
-
-function speak(text){
-    var utterThis = new SpeechSynthesisUtterance(text);
-    utterThis.pitch = 1; //тон
-    utterThis.rate = 1; //скорость
-    synth.speak(utterThis);
-}
